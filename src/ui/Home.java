d850bcf1ce08bb6e44d3ca5dbf9cc4cbc02a9fa1
S4ltF1sh <sankatana100@gmail.com>
2022-10-17 17:45:02 +0700

    Add logic to open popup menu follow mouse right click

--- src/ui/Home.java
+++ src/ui/Home.java
@@ -1,7 +1,10 @@
 package ui;
 
+import ui.popupmenu.FolderPopClickListener;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class Home {
     private JPanel homePanel;
@@ -20,18 +23,25 @@ public class Home {
     private void setUpUI() {
         homePanel.setPreferredSize(new Dimension(1280, 720));
         setUpIcons();
+        setUpOnClick();
+    }
+
+    private void setUpOnClick() {
+        treeBrowse.addMouseListener(new FolderPopClickListener());
+        tableCurrentFolder.addMouseListener(new FolderPopClickListener());
     }
 
     private void setUpIcons() {
-        btBack.setIcon(new ImageIcon(getClass().getResource("/res/drawable/ic_back.png")));
-        btForward.setIcon(new ImageIcon(getClass().getResource("/res/drawable/ic_forward.png")));
-        btUp.setIcon(new ImageIcon(getClass().getResource("/res/drawable/ic_up.png")));
-        btPicker.setIcon(new ImageIcon(getClass().getResource("/res/drawable/ic_open_folder.png")));
+        btBack.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/drawable/ic_back.png"))));
+        btForward.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/drawable/ic_forward.png"))));
+        btUp.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/drawable/ic_up.png"))));
+        btPicker.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/res/drawable/ic_open_folder.png"))));
     }
 
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Home");
+        frame.addMouseListener(new FolderPopClickListener());
         frame.setContentPane(new Home().homePanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
