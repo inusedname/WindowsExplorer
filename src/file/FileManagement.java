f08ea286a922f9294ad43108b60ef566a7a09cdc
DungTheRipper <you@domain.com>
2022-10-25 22:36:58 +0700

    File Management

---
+++ src/file/FileManagement.java
@@ -0,0 +1,104 @@
+package file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileManagement {
+
+    public void deleteFile(String inputPath, String inputFile) {
+        try {
+            // delete the original file
+            new File(inputPath + inputFile).delete();
+        } catch (Exception e) {
+            //Log.e("tag", e.getMessage());
+        }
+    }
+
+    public void renameFile(String inputPath, String inputFile, String outputFile) {
+        try {
+            // rename the original file
+            new File(inputPath + inputFile).renameTo(new File(inputPath + outputFile));
+        } catch (Exception e) {
+            //Log.e("tag", e.getMessage());
+        }
+    }
+
+    public void moveFile(String inputPath, String inputFile, String outputPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File(outputPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file
+            out.flush();
+            out.close();
+            out = null;
+
+            // delete the original file
+            new File(inputPath + inputFile).delete();
+
+        } catch (FileNotFoundException fnfe1) {
+            //Log.e("tag", fnfe1.getMessage());
+        } catch (Exception e) {
+            //Log.e("tag", e.getMessage());
+        }
+    }
+
+    public void copyFile(String inputPath, String inputFile, String outputPath) {
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File(outputPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;
+
+        } catch (FileNotFoundException fnfe1) {
+            //Log.e("tag", fnfe1.getMessage());
+        } catch (Exception e) {
+            //Log.e("tag", e.getMessage());
+        }
+
+    }
+
+}
+
